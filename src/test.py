5bc1ac3dbe2d62b1d39287919aa646353717f168
francose <elmanoukdesign@gmail.com>
2018-04-08 02:19:08 -0400

    clear first and second line in data set manually, Then deleted the previous functions and replace them with pandas built-in methods. '

--- src/test.py
+++ src/test.py
@@ -4,50 +4,41 @@ date : April/6th/2018
 Purpose : it is a temporarly test file to mock the data set.
 '''
 
-#core modules
+# core modules
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#supporting modules
-import csv, json
+# supporting modules
+import csv
+import json
 
 
-
-#gloabls
+# gloabls
 raw_data = '../data/CrashData.csv'
 
-#preprocessing
-def get_column_names(num=[]):
-    with open(raw_data, 'r') as datas:
-        reader = csv.reader(datas)
-        for i, row in enumerate(reader):
-            if (i == 0):
-                first = ''.join(row).split()
-                lenght_first = len(first)
-                # print(first, lenght_firt)
-            num.append(len(''.join(row).split()))
-        m = max(num)
-        rng = range(1, m - lenght_first + 2)
-        return rng
+#Test correlation for the preprocessing step.
+# preprocessing
+def preprocessing():
+    dataframe = pd.read_csv(raw_data)
+    df = pd.DataFrame(dataframe)
+    dropped = df.dropna(axis=1, how="any")
+    correlation = dropped.corr(method='pearson')
+    print(correlation)
 
-
-def processing__(n=30):
-    column_names = get_column_names()
-    #post processing
-    df = pd.read_csv(raw_data, sep="\s+", names=get_column_names(), index_col=[0], skiprows=1)
-    df1 = pd.read_csv(raw_data, sep='\s+', names=range(n))
-    #removes all NaN values
-    df1 = df1.dropna(axis=1, how='all')
-    df1.columns = df1.loc[0].dropna()[:1].append(pd.Series(range(1, len(df1.columns) - len(df1.loc[0].dropna()[:-1]) + 1 )))
-    df1 = df1.ix[1:]
-
-    print(df1.head())
 
 
 
 def main():
-    process = processing__()
+
+    correlation = preprocessing()
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
