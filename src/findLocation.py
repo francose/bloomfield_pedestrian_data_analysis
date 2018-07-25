8fbc765ecef04ff72a9171f0ca0906c43a5163f6
francose <elmanoukdesign@gmail.com>
2018-07-24 22:59:04 -0400

    modulerized

--- src/findLocation.py
+++ src/findLocation.py
@@ -3,42 +3,69 @@ Author : Sadik Erisen
 
 '''
 
-
 # core modules
 import pandas as pd
 import numpy as np
 from pandas.compat import StringIO
-
-
 # supporting modules
 import csv, json, requests,time
 
+READ_DATA_PATH = "../data/paramusdata.csv"
+POST_URL = "http://maps.google.com/maps/api/geocode/json?address="
 
-temp=u"""a,b
-None,NaN
-a,8"""
+def extractColumn():
+    df = pd.read_csv(READ_DATA_PATH, keep_default_na=False,na_values=['NaN'])
+    CRASH_LOCATION = df.CRASH_LOCATION
+    return CRASH_LOCATION
 
+def createFile(data):
+    with open('./static/locationsss.json', 'w') as file:
+        json.dump(data ,file)
 
+def URL_POSTFIX():
+    data = {}
+    data['URL'] = []
+    locations = extractColumn()
+    for i in locations:
+        post_fix_url = POST_URL+ i + ", paramus"
+        data['URL'].append({
+        'paramus':post_fix_url
+        })
+    return data['URL']
 
-Path = "../data/paramusdata.csv"
-url = "http://maps.google.com/maps/api/geocode/json?address="
+def Location_Result():
+    urls = URL_POSTFIX()
+    for i in urls:
+        url = i['paramus']
+        try:
+            request = requests.post(url)
+            print request
+        except ValueError:
+            print requests.raise_for_status()
+    return request
 
 def main():
+    results = Location_Result()
+    print results
 
-    df = pd.read_csv(Path, keep_default_na=False,na_values=['NaN'])
-    new_dataframe = df['CRASH_LOCATION']
-    for i in new_dataframe:
-        newURL = url+i+", paramus"
-        # print newURL
-        time.sleep(.90)
-        r = requests.post(newURL).json()
-        server_message = requests.codes.ok
-        if server_message == 200:
-            file = open('locations.json', 'w')
-            json.dump(r,file)
-        else:
-            r.raise_for_status()
-
+    # storeDta = createFile()
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+# for i in new_dataframe:
+#     newURL = url+i+", paramus"
+#     # print newURL
+#     time.sleep(.90)
+#     r = requests.post(newURL)
+#     json_data = r.json()
+#     for x in json_data:
+#         s =json_data['results']
+#         json.dump(s, file)
